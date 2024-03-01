f429d101323897c6022711d08318948c735fe4e1
klamer.dev <161390980+KL4M3R@users.noreply.github.com>
2024-03-01 07:27:08 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -2,12 +2,9 @@ ESX = nil
 
 local robbedRecently = false
 local graj_animke_npc = false
-Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
-    end
-end)
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 
 
